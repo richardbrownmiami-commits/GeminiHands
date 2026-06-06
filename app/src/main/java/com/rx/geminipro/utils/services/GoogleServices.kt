c4f4f4ed7c788d1b00919abc4aa6f17f1358ecc1
richardbrownmiami-commits <richard.brown.miami@gmail.com>
2026-06-06 16:09:33 +0530

    Fix GoogleServices: add @Inject constructor()

--- app/src/main/java/com/rx/geminipro/utils/services/GoogleServices.kt
+++ app/src/main/java/com/rx/geminipro/utils/services/GoogleServices.kt
@@ -3,8 +3,11 @@ package com.rx.geminipro.utils.services
 import android.content.Context
 import android.content.Intent
 import androidx.core.net.toUri
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class GoogleServices {
+@Singleton
+class GoogleServices @Inject constructor() {
     fun openGoogleDocs(context: Context) {
         val googleDocsUrl = "https://docs.google.com/document/create"
         val intent = Intent(Intent.ACTION_VIEW, googleDocsUrl.toUri())
